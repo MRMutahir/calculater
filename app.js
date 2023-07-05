5c4ecc55330cacdea232edc3ea6118a84e395b6c
Yahiya Tufail <Yahiya Tufail>
2023-07-05 12:41:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+let inPut = document.getElementById('inPut');
+// let equal = document.getElementById('equal');
+let button = document.querySelector('button');
+// console.log(button.value);
+
+
+// function numbers(getnum) {
+//     inPut.value += getnum 
+//     equal(inPut.value += getnum )
+// }
+function equal(a) {
+    console.log('hi');
+    // console.log(a);
+    function numbers(getnum) {
+        inPut.value += getnum 
+        // equal(inPut.value += getnum )
+    }
+}
+
+
